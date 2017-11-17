401053ab01f77551d5f4ac11e902611938640d09
Sandeep Anand <anandjairam@ufl.edu>
2017-11-16 20:21:50 -0800

    Update FindFinalIndex.py
    Output For this : 1

--- FindFinalIndex.py
+++ FindFinalIndex.py
@@ -1,34 +1,27 @@
-def findFinalIndex(nums, index, K):
-    """
-    type nums: an array of integers of length N. Values are in the range of 0...N-1
-    type index: int
-    type K: int
-    """
-    #observation: there exists one and only one cycle from any given index
-    seen = set()
-    cycle = []
-    pivot = None
-
-    while K > 0:
-        K-=1
-        if index == pivot:
-            K += 1
-            break
-        if index in seen:
-            if pivot == None:
-                pivot = index
-            cycle.append(index)
-
-        seen.add(index)
-        index = nums[index]
-    if K == 0:      # no cycle found
-        return index
-    print ("Cycle found: ", cycle)
-    print ("The number of steps left is %d" % K)
-    return cycle[K % len(cycle)]
+class Solution(object):
+    """docstring for Solution."""
+    def __init__(self):
+        super(Solution, self).__init__()
+    def returnindex2(self, nums, k, si):
+            n,cycle = len(nums),[]
+            if si<0 or si>(n-1):
+                return -1
+            else:
+                nextstep = si
+                cycle.append(si)
+                while k!=0:
+                    nextstep = nums[nextstep]
+                    if nextstep in cycle:
+                        #cycle detected
+                        break
+                    cycle.append(nextstep)
+                    k-=1
+                #print("debug",nextstep,k,cycle)
+                if k==0:
+                    return nextstep
+                else:
+                    return cycle[-1] if k%2==0 else cycle[-2]
 
 
-nums = [1,3,0,1,4,2]
-index = 1
-K = 5000000
-print (findFinalIndex(nums, index, K))
+obj=Solution()
+print(obj.returnindex2([1,3,0,1,4,2], 500000, 2))
